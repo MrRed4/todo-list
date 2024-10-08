3ba12c38a4011aed039cf95bcf7d990460edc26f
mrred <coopk2e@gmail.com>
2024-10-08 20:16:20 +1100

    Reformat code

--- src/index.js
+++ src/index.js
@@ -34,17 +34,111 @@ const ProjectList = (() => {
     return { addProject, getProjects, removeProject, addTodo, numberOfTasks, updateList }
 })()
 
-const newProjectBtn = document.querySelector('.new-project')
-const newTodoBtn = document.querySelector('.new-todo')
-const confirmNewProject = document.querySelector('#confirmProjectBtn')
-const confirmNewTodo = document.querySelector('#confirmTodoBtn')
+createDefault()
+ProjectList.updateList()
 
-newProjectBtn.addEventListener('click', () => {document.querySelector('#project-dialog').showModal()})
-confirmNewProject.addEventListener('click', submitProject)
+const domController = (() => {
 
-newTodoBtn.addEventListener('click', () => {document.querySelector('#todo-dialog').showModal()})
-confirmNewTodo.addEventListener('click', submitTask)
+    const newProjectBtn = document.querySelector('.new-project')
+    const newTodoBtn = document.querySelector('.new-todo')
+    const confirmNewProject = document.querySelector('#confirmProjectBtn')
+    const confirmNewTodo = document.querySelector('#confirmTodoBtn')
 
+    newProjectBtn.addEventListener('click', () => {document.querySelector('#project-dialog').showModal()})
+    confirmNewProject.addEventListener('click', submitProject)
+
+    newTodoBtn.addEventListener('click', () => {document.querySelector('#todo-dialog').showModal()})
+    confirmNewTodo.addEventListener('click', submitTask)
+
+    function renderProjects() {
+        const projectBar = document.querySelector('#projects')
+        const projectDesc = document.querySelector('.project-description')
+        projectBar.innerHTML = ''
+    
+        ProjectList.getProjects().forEach((project, index) => {
+            const tab = document.createElement('button')
+            tab.addEventListener('click', changeActiveProject)
+            tab.classList = index
+            tab.textContent = `${project.title} [${project.numberOfTasks}]`
+            tab.style = `border-bottom: 4px solid ${project.colour}`
+            projectBar.appendChild(tab)
+    
+            if (project.isActive) {
+                projectDesc.textContent = project.desc
+                tab.classList.add('active')
+            } 
+        })
+    }
+
+    function renderTodoItems() {
+        const container = document.querySelector('.todo-items')
+        container.innerHTML = ''
+    
+        ProjectList.getProjects().forEach((project) => {
+            if (project.isActive) {
+                project.tasks.forEach((task, index) => {
+                    const x = document.createElement('button')
+                    const card = document.createElement('div')
+                    const title = document.createElement('h3')
+                    const description = document.createElement('p')
+                    const dueDate = document.createElement('p')
+                    const priority = document.createElement('p')
+                    const topDiv = document.createElement('div')
+                    const botDiv = document.createElement('div')
+    
+                    x.textContent = 'X'
+                    title.textContent = `${task.title}`
+                    description.textContent = `${task.desc}`
+                    dueDate.textContent = `Due: ${task.dueDate}`
+                    priority.textContent = `Priority: ${task.prio}`
+    
+                    card.classList = `card ${index} ${task.prio}`
+                    x.classList = `${index}`
+                    botDiv.classList = 'task-info'
+                    
+                    container.appendChild(card)
+                    card.appendChild(topDiv)
+                    topDiv.appendChild(title)
+                    topDiv.appendChild(x)
+                    card.appendChild(description)
+                    card.appendChild(botDiv)
+                    botDiv.appendChild(priority)
+                    botDiv.appendChild(dueDate)
+    
+                    x.addEventListener('click', removeTask)
+                })
+            }
+        })
+        renderProjects()
+    }
+
+    function changeActiveProject(e) {
+        ProjectList.getProjects().forEach((project) => {
+            project.isActive = false
+        })
+        ProjectList.getProjects()[e.target.classList[0]].toggleActive()
+        render()
+    }
+    
+    function removeTask(e) {
+        ProjectList.getProjects().forEach((project) => {
+            if (project.isActive) {
+                project.removeTodo(e.target.classList.value)
+            }
+        })
+        render()
+    }
+
+    function render() {
+        renderTodoItems()
+        renderProjects()
+    }
+
+    // Initial render
+    render()
+
+    return { renderTodoItems, renderProjects }
+})()
 
 function submitProject() {
     const projectName = document.querySelector('#new-project-name')
@@ -52,7 +146,7 @@ function submitProject() {
     const projectColour = document.querySelector('#new-project-colour')
 
     createProject(projectName.value, projectDescription.value, projectColour.value)
-    renderProjects()
+    domController.renderProjects()
 
     projectName.value = ''
     projectDescription.value = ''
@@ -82,7 +176,7 @@ function submitTask() {
     const taskPrio = document.querySelector('#new-task-priority')
 
     createTask(taskName.value, taskDescription.value, taskDueDate.value, taskPrio.value, new Date().toLocaleDateString("en-AU"))
-    renderTodoItems()
+    domController.renderTodoItems()
 
     taskName.value = ''
     taskDescription.value = ''
@@ -106,90 +200,6 @@ function createDefault() {
     populateStorage(defaultProject)
 }
 
-function renderProjects() {
-    const projectBar = document.querySelector('#projects')
-    const projectDesc = document.querySelector('.project-description')
-    projectBar.innerHTML = ''
-
-    ProjectList.getProjects().forEach((project, index) => {
-        const tab = document.createElement('button')
-        tab.addEventListener('click', changeActiveProject)
-        tab.classList = index
-        tab.textContent = `${project.title} [${project.numberOfTasks}]`
-        tab.style = `border-bottom: 4px solid ${project.colour}`
-        projectBar.appendChild(tab)
-
-        if (project.isActive) {
-            projectDesc.textContent = project.desc
-            tab.classList.add('active')
-        } 
-    })
-}
-
-function renderTodoItems() {
-    const container = document.querySelector('.todo-items')
-    container.innerHTML = ''
-
-    ProjectList.getProjects().forEach((project) => {
-        if (project.isActive) {
-            project.tasks.forEach((task, index) => {
-                const x = document.createElement('button')
-                const card = document.createElement('div')
-                const title = document.createElement('h3')
-                const description = document.createElement('p')
-                const dueDate = document.createElement('p')
-                const priority = document.createElement('p')
-                const topDiv = document.createElement('div')
-                const botDiv = document.createElement('div')
-
-                x.textContent = 'X'
-                title.textContent = `${task.title}`
-                description.textContent = `${task.desc}`
-                dueDate.textContent = `Due: ${task.dueDate}`
-                priority.textContent = `Priority: ${task.prio}`
-
-                card.classList = `card ${index} ${task.prio}`
-                x.classList = `${index}`
-                botDiv.classList = 'task-info'
-                
-                container.appendChild(card)
-                card.appendChild(topDiv)
-                topDiv.appendChild(title)
-                topDiv.appendChild(x)
-                card.appendChild(description)
-                card.appendChild(botDiv)
-                botDiv.appendChild(priority)
-                botDiv.appendChild(dueDate)
-
-                x.addEventListener('click', removeTask)
-            })
-        }
-    })
-    renderProjects()
-}
-
-function changeActiveProject(e) {
-    ProjectList.getProjects().forEach((project) => {
-        project.isActive = false
-    })
-    ProjectList.getProjects()[e.target.classList[0]].toggleActive()
-    render()
-}
-
-function removeTask(e) {
-    ProjectList.getProjects().forEach((project) => {
-        if (project.isActive) {
-            project.removeTodo(e.target.classList.value)
-        }
-    })
-    render()
-}
-
-function render() {
-    renderTodoItems()
-    renderProjects()
-}
-
 function populateStorage(project) {
     localStorage.setItem(`${project.title}`, JSON.stringify(project))
 }
@@ -197,7 +207,3 @@ function populateStorage(project) {
 function getStorage() {
     return JSON.stringify(localStorage)
 }
-
-createDefault()
-ProjectList.updateList()
-render()
